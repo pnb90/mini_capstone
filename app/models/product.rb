2a8ed17430d92152ba8030284bda2808f27799f5
pnb90 <phuocbui90@gmail.com>
2019-01-21 13:32:56 -0600

    partials /w updated view/index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    # if price < 4
+    #   true
+    # else 
+    #   false
+    # end 
+
+    price < 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+  
 end
