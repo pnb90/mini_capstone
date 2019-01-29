867854a8f099bccb1e4e7643a0f7141a4bf7bac1
pnb90 <phuocbui90@gmail.com>
2019-01-29 11:35:40 -0600

    added JWT tokens and backend authentification

--- app/views/api/products/show.json.jbuilder
+++ app/views/api/products/show.json.jbuilder
@@ -1,1 +1,2 @@
+json.current_user current_user
 json.partial! @product, partial: "product", as: :product
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
+    post '/users' => 'users#create'
+
+    post '/sessions' => 'sessions#create'
+    
     get '/products' => 'products#index'
-
     get '/products/:id' => 'products#show'
-
     post '/products' => 'products#create'
-
     patch '/products/:id' => 'products#update'
-
     delete 'products/:id' => 'products#destroy'
   end
 end
